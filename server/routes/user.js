72986a5984364493508e30e9e89b4d4dcfea9a50
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-02-08 06:47:44 +0800

    Add user js routes for sign in and sign up

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+import { signUp, signIn } from "../controllers/user.js";
+
+router.post("/signUp", signUp);
+router.post("/signIn", signIn);
+
+export default router;
